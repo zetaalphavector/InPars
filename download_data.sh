2753ba4dc60fdc4d3e3346a6ace8be2189b5132d
Luiz Bonifacio <luizhbonifacio@gmail.com>
2023-01-27 14:38:15 -0400

    Update download_data.sh

--- download_data.sh
+++ download_data.sh
@@ -9,6 +9,6 @@ mkdir -p $DATA
 wget -P $DATA https://msmarco.blob.core.windows.net/msmarcoranking/collection.tar.gz
 wget -P $DATA https://raw.githubusercontent.com/castorini/anserini/master/src/main/resources/topics-and-qrels/topics.msmarco-passage.dev-subset.txt
 wget -P $DATA https://raw.githubusercontent.com/castorini/anserini/master/src/main/resources/topics-and-qrels/qrels.msmarco-passage.dev-subset.txt
-gsutil cp gs://nm_datasets/trec_covid/runs/run.beir-v1.0.0-trec-covid-flat.trec $DATA
+wget -P $DATA https://storage.googleapis.com/duobert_git/run.bm25.dev.small.tsv
 tar -xf $DATA/collection.tar.gz -C $DATA
 rm $DATA/collection.tar.gz

055d678dfca17f3130cbb66b3c23edee82cee285
Hugo Abonizio <hugo.abonizio@gmail.com>
2023-03-27 16:55:14 -0500

    Bump version

--- setup.py
+++ setup.py
@@ -2,14 +2,15 @@ from setuptools import find_packages, setup
 
 setup(
    name='inpars',
-   version='0.1.0',
+   version='0.2.1',
    author='Hugo Abonizio',
    author_email='hugo.abonizio@gmail.com',
-   packages=find_packages("inpars"),
+   packages=find_packages(),
    url='https://github.com/zetaalphavector/InPars',
    license='LICENSE.txt',
    description='InPars',
    long_description=open('README.md').read(),
+   long_description_content_type='text/markdown',
    install_requires=[
       'torch',
       'pyserini>=0.19',
